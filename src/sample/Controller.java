ba3e54ecea67c64aa8ea8463d1853701385f89a7
rlated1337 <rlated@protonmail.com>
2018-12-11 18:54:02 +0100

    Basic Mockup, Test Methods, Controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,28 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+
 public class Controller {
+    // VIEWS
+    @FXML private TextField colorInput, startCurve;
+    @FXML private Button drawButton;
+
+
+    public Main main;
+
+
+    public void setMain(Main main){
+        this.main = main;
+    }
+
+    @FXML
+    public void testMethod(){
+        String color = colorInput.getText();
+        int start_curve = Integer.parseInt(startCurve.getText());
+
+        System.out.println("Color: " + color);
+        System.out.println("Start Kurve: " + start_curve);
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,31 +4,45 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.*;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.*;
 import javafx.scene.shape.Rectangle;
 
+import java.io.IOException;
+
 public class Main extends Application {
+
+    private Stage primaryStage;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-
-        Rectangle rectangle = new Rectangle();
-
-        rectangle.setX(100.0);
-        rectangle.setY(150.0);
-        rectangle.setWidth(500.0);
-        rectangle.setHeight(150.0);
-
-        Group root2 = new Group(rectangle);
-        primaryStage.setScene(new Scene(root2, 300, 275));
+        this.primaryStage = primaryStage;
+        mainWindow();
 
 
-        primaryStage.show();
     }
 
+    public void mainWindow(){
+        try{
+            FXMLLoader loader = new FXMLLoader(Main.class.getResource("sample.fxml"));
+            AnchorPane pane = loader.load();
+
+            primaryStage.setMinHeight(800.00);
+            primaryStage.setMinWidth(600.00);
+
+            Controller windowController = loader.getController();
+            windowController.setMain(this);
+
+            Scene scene = new Scene(pane);
+
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+
+    }
 
     public static void main(String[] args) {
         launch(args);
