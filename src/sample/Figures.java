7cfa0eeadabe99c68e8c7624efb7ce84a150d324
Huncho6 <adnansoftic3110>
2018-12-13 17:23:07 +0100

    shapes in figures

--- src/sample/Figures.java
+++ src/sample/Figures.java
@@ -16,6 +16,12 @@ public class Figures {
             case 1:
                 line(gc);
                 break;
+            case 2:
+                triangle(gc);
+                break;
+            case 5:
+                circle(gc);
+                break;
             default:
                 // THROW ERROR
                 break;
@@ -28,6 +34,21 @@ public class Figures {
         System.out.println("line");
         gc.strokeLine(200,250,600,250);
     }
+    private void circle(GraphicsContext gc){
+        System.out.println("circle");
+        gc.strokeOval(100,150,500,150);
+    }
+    private void triangle(GraphicsContext gc){
+        System.out.println("triangle");
+        gc.beginPath();
+        gc.moveTo(30.5, 30.5);
+        gc.lineTo(150.5, 30.5);
+        gc.lineTo(150.5, 150.5);
+        gc.lineTo(30.5, 30.5);
+        gc.stroke();
+    }
+
+
 
     private void mandelbrot(){}
 
