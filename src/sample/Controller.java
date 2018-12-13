2e584c915b6214cd0b1d663e39596f20b187de56
rlated1337 <rlated@protonmail.com>
2018-12-13 10:35:01 +0100

    drawing, button action

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,11 +3,16 @@ package sample;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.paint.Color;
+
 
 public class Controller {
     // VIEWS
     @FXML private TextField colorInput, startCurve;
     @FXML private Button drawButton;
+    @FXML private Canvas drawArea;
 
 
     public Main main;
@@ -19,10 +24,22 @@ public class Controller {
 
     @FXML
     public void testMethod(){
-        String color = colorInput.getText();
+       /* String color = colorInput.getText();
         int start_curve = Integer.parseInt(startCurve.getText());
 
         System.out.println("Color: " + color);
         System.out.println("Start Kurve: " + start_curve);
+       */
+
+        GraphicsContext gc = drawArea.getGraphicsContext2D();
+        drawShapes(gc);
+    }
+
+    public void drawShapes(GraphicsContext gc) {
+        gc.setFill(Color.GREEN);
+        gc.setStroke(Color.BLUE);
+        gc.setLineWidth(5);
+        gc.strokeLine(40, 10, 10, 40);
+        gc.fillOval(10, 60, 30, 30);
     }
 }
